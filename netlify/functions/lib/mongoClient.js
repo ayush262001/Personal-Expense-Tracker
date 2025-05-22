8cba6243a7bbb486b370c2a616358dd534530b3a
Ayush Gour <ayushofficials2609@gmail.com>
2025-05-23 00:32:02 +0530

    package.json updated 2

--- netlify/functions/lib/mongoClient.js
+++ netlify/functions/lib/mongoClient.js
@@ -4,10 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const uri = process.env.MONGO_URI;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const client = new MongoClient(uri);
 
 let cachedDb = null;
 
