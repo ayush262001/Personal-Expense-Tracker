6bfd25bdb8c0139629b0ff75bc6d1030ec073fb5
Ayush Gour <ayushofficials2609@gmail.com>
2025-05-27 00:51:03 +0530

    expense add filter 3 added

--- netlify/functions/getExpenseList.js
+++ netlify/functions/getExpenseList.js
@@ -33,7 +33,7 @@ exports.handler = async (event) => {
     const filterRaw = params.filter || '0';
     const filter = parseInt(filterRaw, 10);
 
-    if (![0, 1, 2].includes(filter)) {
+    if (![0, 1, 2, 3].includes(filter)) {
       return {
         statusCode: 400,
         headers: { 'Access-Control-Allow-Origin': '*' },
@@ -57,7 +57,27 @@ exports.handler = async (event) => {
       startDate.setDate(now.getDate() - 90);
     }
 
-    const transactions = await db
+    if(filter===3){
+      const transactions = await db
+      .collection('transactions')
+      .find({
+        userId: new ObjectId(userId),
+      })
+      .sort({ createdAt: -1 })
+      .toArray();
+
+      return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Authorization, Content-Type',
+      },
+      body: JSON.stringify({ transactions }),
+    };
+    }
+
+    else{
+      const transactions = await db
       .collection('transactions')
       .find({
         userId: new ObjectId(userId),
@@ -66,7 +86,7 @@ exports.handler = async (event) => {
       .sort({ createdAt: -1 })
       .toArray();
 
-    return {
+      return {
       statusCode: 200,
       headers: {
         'Access-Control-Allow-Origin': '*',
@@ -74,6 +94,9 @@ exports.handler = async (event) => {
       },
       body: JSON.stringify({ transactions }),
     };
+    }
+
+    
   } catch (error) {
     console.error('Error fetching transactions by timeframe:', error);
     return {
